b50d6cd9e4efda0a836c7216ba953f9e127f4308
adityafawzan <fawzan89@gmail.com>
2022-08-03 15:53:51 +0700

    03-08-22-1553

--- src/palindrome.ts
+++ src/palindrome.ts
@@ -1,9 +1,8 @@
 export const isPalindrome = (str: string) => {
   const str_len = str.length;
-  const perulangan = Math.ceil(str_len / 2);
 
-  for (let i = 1; i <= perulangan; i++) {
-    if (str.charAt(i - 1) !== str.charAt(str_len - i)) {
+  for (let i = 0; i < str_len; i++) {
+    if (str.charAt(i) !== str.charAt(str_len - 1 - i)) {
       return false;
     }
   }
